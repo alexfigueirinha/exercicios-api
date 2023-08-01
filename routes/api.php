9138be71e7055be5e446fe8e53bc8c2314d60b2d
alexfigueirinha <alex.figueirinha@sp.senai.br>
2023-08-01 13:46:42 -0300

    Exercício 7

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\ExercicioDoisController;
+use App\Http\Controllers\ExercicioSeteController;
 use App\Http\Controllers\ExercicioUmController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('exercicio/um', [ExercicioUmController::class, 'multiplicar']);
 
 Route::post('exercicio/dois', [ExercicioDoisController::class, 'retornarMaior']);
+
+Route::get('exercicio/sete', [ExercicioSeteController::class, 'exibirNumeros']);
